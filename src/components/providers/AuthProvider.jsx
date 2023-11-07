f4e09af8c2e4603a5d9ee1f51b4eaec84e581231
searchsakib <searchsakib@gmail.com>
2023-11-07 17:04:43 +0600

    firebase initiated, authprovider added

---
+++ src/components/providers/AuthProvider.jsx
@@ -0,0 +1,61 @@
+// import { createContext, useEffect, useState } from 'react';
+// import app from '../Firebase/firebase.config';
+
+import { createContext, useEffect, useState } from 'react';
+import app from '../Firebase/firebase.config';
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password, name, photo) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password, name, photo);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log('user in the auth state changed', currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import myRoute from './Router/Route';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './components/providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={myRoute}></RouterProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={myRoute}></RouterProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );

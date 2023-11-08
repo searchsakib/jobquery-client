10ffe6746d1194b77e500fc935e88494a03e6e66
searchsakib <searchsakib@gmail.com>
2023-11-08 07:23:41 +0600

    aos scroll animation added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,29 @@ import Banner from '../../components/Banner/Banner';
 import Services from '../../components/Services/Services';
 import HireExperts from '../../components/HireExperts/HireExperts';
 import Career from '../../components/Career/Career';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  // for aos
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="pb-[100px] pt-[100px] overflow-x-hidden">
       <Helmet>
         <title>Job Quest | Home</title>
       </Helmet>
       <Banner></Banner>
-      <div className="pb-[100px] pt-[100px]">
+      <div className="pb-[100px] pt-[100px]" data-aos="zoom-in-up">
         <Services></Services>
       </div>
-      <div className="pb-[100px]">
+      <div className="pb-[100px]" data-aos="zoom-out-right">
         <HireExperts></HireExperts>
       </div>
-      <div>
+      <div data-aos="zoom-out-left">
         <Career></Career>
       </div>
     </div>

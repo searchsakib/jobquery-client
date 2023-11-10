ce7ac5579061a386d2e4a4771dce2051b05ca2e4
searchsakib <searchsakib@gmail.com>
2023-11-11 04:49:30 +0600

    empty page msg added

--- src/pages/MyBids/MyBids.jsx
+++ src/pages/MyBids/MyBids.jsx
@@ -28,59 +28,73 @@ const MyBids = () => {
   // console.log(allBids);
 
   return (
-    <div className="max-w-screen-xl mx-auto my-20">
+    <div className="max-w-screen-xl mx-auto my-20 min-h-[34vh]">
       <Helmet>
         <title>Job Quest | My Bids</title>
       </Helmet>
 
-      <h2 className="text-center text-3xl font-medium pb-14">My Bids</h2>
-      <div className="overflow-x-auto">
-        <table className="divide-y-2 divide-gray-200 bg-white text-sm mx-auto w-8/12 min-w-fit ">
-          <thead className="text-left">
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
-                Job Title
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
-                Buyer Email
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
-                Deadline
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
-                Status
-              </th>
-              <th className="px-4 py-2"></th>
-            </tr>
-          </thead>
-          {myBids.map((myBid) => (
-            <tbody key={myBid._id} className="divide-y divide-gray-200">
+      {myBids?.length > 0 ? (
+        <div className="overflow-x-auto">
+          <h2 className="text-center text-3xl font-medium pb-14">My Bids</h2>
+          <table className="divide-y-2 divide-gray-200 bg-white text-sm mx-auto w-8/12 min-w-fit ">
+            <thead className="text-left">
               <tr>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-base">
-                  {myBid.job_title}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
-                  {myBid.buyer}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
-                  {myBid.deadline}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
-                  {completed}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2">
-                  <Link
-                    onClick={() => handleComplete()}
-                    className={`uppercase ${displayBtn} rounded bg-[#2161a2] px-4 py-2 text-sm font-medium text-white hover:bg-[#1b4978]`}
-                  >
-                    Complete
-                  </Link>
-                </td>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
+                  Job Title
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
+                  Buyer Email
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
+                  Deadline
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-lg">
+                  Status
+                </th>
+                <th className="px-4 py-2"></th>
               </tr>
-            </tbody>
-          ))}
-        </table>
-      </div>
+            </thead>
+            {myBids.map((myBid) => (
+              <tbody key={myBid._id} className="divide-y divide-gray-200">
+                <tr>
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 text-base">
+                    {myBid.job_title}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
+                    {myBid.buyer}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
+                    {myBid.deadline}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700 text-base">
+                    {completed}
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2">
+                    <Link
+                      onClick={() => handleComplete()}
+                      className={`uppercase ${displayBtn} rounded bg-[#2161a2] px-4 py-2 text-sm font-medium text-white hover:bg-[#1b4978]`}
+                    >
+                      Complete
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-10 px-10 pt-20 pb-10">
+          <h2 className="text-3xl text-center font-medium">No bid Yet...</h2>
+          <Link
+            to="/"
+            rel="noopener noreferrer"
+            href="#"
+            className="px-8 py-3 font-semibold rounded bg-[#05386B] text-white hover:outline  hover:bg-white hover:text-[#05386B] text-center mx-auto"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      )}
     </div>
   );
 };

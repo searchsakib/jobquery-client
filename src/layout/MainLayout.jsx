e6f2d3ccfd1989fc5f6fe9a0984d3a0af3b87ab7
searchsakib <searchsakib@gmail.com>
2024-04-14 07:02:45 +0600

    reduced website zoom to 90%

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
+import { useEffect } from 'react';
 
 const MainLayout = () => {
+  useEffect(()=>{
+    document.body.style.zoom="90%"
+  },[])
   return (
     <div>
       <div className="bg-[#05386B]">
